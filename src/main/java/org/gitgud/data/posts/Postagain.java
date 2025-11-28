6fc6bbdb700b5d7a16b01a39ee3714dc349a8d48
Eyyy_G <agsamonte27@gmail.com>
2025-11-28 08:09:53 +0000

    create a new post file daw

---
+++ src/main/java/org/gitgud/data/posts/Postagain.java
@@ -0,0 +1,16 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+// Replace ExamplePost with the name of your .java file
+public class Postagain extends StaticPost {
+    // Replace ExamplePost here as well
+    public Postagain() {
+        // Enter your message inside the triple quotes
+        super("""
+                Hi b-
+                I'm eyyy_g!
+                """, true); // You can set wrap to false if you prefer
+    }
+}
