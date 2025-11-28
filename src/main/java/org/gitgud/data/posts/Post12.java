74babc540282e0938171b7977bcd3f20d681dab8
anen <neniabarao0@gmail.com>
2025-11-28 08:10:13 +0000

    heu anue daw?

---
+++ src/main/java/org/gitgud/data/posts/Post12.java
@@ -0,0 +1,11 @@
+// Replace ExamplePost with the name of your .java file
+public class Post12 extends StaticPost {
+    // Replace ExamplePost here as well
+    public Post12() {
+        // Enter your message inside the triple quotes
+        super("""
+                Na para bang
+                Thank you, aa.
+                """, true); // You can set wrap to false if you prefer
+    }
+}
