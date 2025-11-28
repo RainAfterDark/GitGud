064cbbe08784dba85b8ed1e71956de1ca612e261
Nurina Lakian <nalakian01202500382@usep.edu.ph>
2025-11-28 07:57:40 +0000

    This is a test

---
+++ src/main/java/org/gitgud/data/posts/Wowie.java
@@ -0,0 +1,13 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+
+public class Wowie extends StaticPost {
+    public Wowie() {
+        super("""
+                Hi.
+                """, true);
+    }
+}
