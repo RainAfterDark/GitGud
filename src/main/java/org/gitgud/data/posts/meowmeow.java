91d0e19102436862e065d322e18a689632c6c952
Vincent Casuyac <vecasuyac01202401552@usep.edu.ph>
2025-11-28 08:09:44 +0000

    meow

---
+++ src/main/java/org/gitgud/data/posts/meowmeow.java
@@ -0,0 +1,16 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+// Replace ExamplePost with the name of your .java file
+public class meowmeow extends StaticPost {
+    // Replace ExamplePost here as well
+    public meowmeow() {
+        // Enter your message inside the triple quotes
+        super("""
+                meowmeowmeowmeowme
+                owmeowmeowmeasowmeow
+                """, true); // You can set wrap to false if you prefer
+    }
+}
