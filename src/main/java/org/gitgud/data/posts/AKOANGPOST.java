91831e9f169bbc0b9ae1865db8089d9113fa111a
cursedclown303-code <cursedclown303@gmail.com>
2025-11-28 08:10:16 +0000

    Charlie kirk Joined

---
+++ src/main/java/org/gitgud/data/posts/AKOANGPOST.java
@@ -0,0 +1,16 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+// Replace ExamplePost with the name of your .java file
+public class AKOANGPOST extends StaticPost {
+    // Replace ExamplePost here as well
+    public AKOANGPOST() {
+        // Enter your message inside the triple quotes
+        super("""
+                MAC LIKES MINORS!!!
+                Git Gud Workshop 2025!
+                """, true); // You can set wrap to false if you prefer
+    }
+}
