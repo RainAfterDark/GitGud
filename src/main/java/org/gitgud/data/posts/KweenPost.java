fcac563822882ebc2c596e88800257eaf7985953
Azi <geroshantaycubio@gmail.com>
2025-11-28 08:12:57 +0000

    hello

---
+++ src/main/java/org/gitgud/data/posts/KweenPost.java
@@ -0,0 +1,16 @@
+package org.gitgud.data.posts;
+
+import org.gitgud.core.model.StaticPost;
+
+@SuppressWarnings("unused")
+// Replace ExamplePost with the name of your .java file
+public class KweenPost extends StaticPost {
+    // Replace ExamplePost here as well
+    public KweenPost() {
+        // Enter your message inside the triple quotes
+        super("""
+                Hello, everyone!
+                Thank you for the Git Gud Workshop 2025!
+                """, true); // You can set wrap to false if you prefer
+    }
+}
